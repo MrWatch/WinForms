d572d7a4edb6cc0f018d29b153ea6325e8182c57
MrWatch <mrhuman00@gmail.com>
2017-09-14 21:39:32 +0600

    Add files via upload

---
+++ 13.09(2)/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Угадайте число от 1 до 2000", "Game");
+            Random rand = new Random();
+            string random;
+            Form1 form = new Form1();
+            form.Hide();
+            for (int i = 0; i < 2000; ++i)
+            {
+                random = Convert.ToString(rand.Next(1, 2000));
+                if (MessageBox.Show(random, Convert.ToString(i++), MessageBoxButtons.OKCancel, MessageBoxIcon.Stop) == DialogResult.OK) {
+                    MessageBox.Show("Количество попыток: " + Convert.ToString(i), "Game is end");
+                    break;
+                }
+                else { continue; }
+            }
+            if(MessageBox.Show("Хотите сыграть снова?", "Game", MessageBoxButtons.RetryCancel) == DialogResult.Retry)
+            {
+                Form1 newform=new Form1();
+                newform.Show();
+            }
+        }
+        
+    }
+}
---
+++ 13.09(2)/WindowsFormsApp3/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("WindowsFormsApp3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WindowsFormsApp3")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("68b7b7bf-0527-41cb-8667-2d773d2441f0")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
