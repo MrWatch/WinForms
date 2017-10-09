c9521a4c818caa5f1be961e58792d2f02142fd04
MrWatch <mrhuman00@gmail.com>
2017-10-09 21:36:19 +0600

    18.09 Date

---
+++ 18.09/WindowsFormsApp7/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace WindowsFormsApp7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textDay = new System.Windows.Forms.TextBox();
+            this.textRes = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(178, 229);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(259, 124);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textDay
+            // 
+            this.textDay.Location = new System.Drawing.Point(178, 116);
+            this.textDay.Name = "textDay";
+            this.textDay.Size = new System.Drawing.Size(259, 20);
+            this.textDay.TabIndex = 1;
+            // 
+            // textRes
+            // 
+            this.textRes.Location = new System.Drawing.Point(178, 391);
+            this.textRes.Name = "textRes";
+            this.textRes.Size = new System.Drawing.Size(259, 20);
+            this.textRes.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(672, 557);
+            this.Controls.Add(this.textRes);
+            this.Controls.Add(this.textDay);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textDay;
+        private System.Windows.Forms.TextBox textRes;
+    }
+}
+
